e8bc80695617d75f432143fad01da6e1ad833d85
BCK <79763943+cicadatree@users.noreply.github.com>
2022-03-29 18:43:37 -0400

    added comments

--- Breadth-First Search.py
+++ Breadth-First Search.py
@@ -18,18 +18,21 @@ visited = [] # initaite list for visited nodes
 queue = [] # initialize list for the queue
 
 def bfs(visited, graph, Firstnode): # breadth-first search function;
+
+    #add your Firstnode to the the visited and queue lists
     visited.append(Firstnode)
     queue.append(Firstnode)
 
     while queue:
-        m = queue.pop(0)
-        print (m, end = " ")
+        m = queue.pop(0) #dequeue the next node in the queue list
+        print (m, end = " ") # print the dequeued node in a single line
 
-        for neighbour in graph[m]:
-            if neighbour not in visited:
+        
+        for neighbour in graph[m]: # iterate over each neighbour of the dequed node
+            if neighbour not in visited:  # if the neighbour hasn't yet been visited, append it to to the visited and queue lists
                 visited.append(neighbour)
                 queue.append(neighbour)
 
 # Driver Code
-print("Following is the Breadth-Frist Search")
-bfs(visited, graph, '3') # call the function
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '3') # call the function at a defined starting node
